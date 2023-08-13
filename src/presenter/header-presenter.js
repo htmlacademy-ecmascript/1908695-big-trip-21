4f4f6bbda355c3e76a4def43006a82ced91cc949
olgadeveloper <madonovaolga@icloud.com>
2023-08-13 15:58:11 +0700

    added header info view and presenter

--- src/presenter/header-presenter.js
+++ src/presenter/header-presenter.js
@@ -1,6 +1,7 @@
-import { render } from '../render.js';
+import { render, RenderPosition } from '../render.js';
 import FormFilterView from '../view/filter-form/filter-form-view.js';
 import FilterItemView from '../view/filter-form/filter-item-view.js';
+import HeaderInfoView from '../view/header-info/header-info-view.js';
 
 const FilterType = {
   EVERYTHING: 'Everything',
@@ -8,12 +9,14 @@ const FilterType = {
   PRESENT: 'Present',
   PAST: 'Past',
 };
-
+const tripMainElement = document.querySelector('.trip-main');
 const tripFiltersContainerElement = document.querySelector('.trip-controls__filters');
 
 export default class HeaderPresenter {
   formFilter = new FormFilterView();
+  headerInfoView = new HeaderInfoView('Amsterdam &mdash; Chamonix &mdash; Geneva', 'Mar 18', 20, 1230);
   init () {
+    render(this.headerInfoView, tripMainElement, RenderPosition.AFTERBEGIN);
     render(this.formFilter, tripFiltersContainerElement);
     Object.values(FilterType).forEach((value) => render(new FilterItemView(value), this.formFilter.getElement()));
   }

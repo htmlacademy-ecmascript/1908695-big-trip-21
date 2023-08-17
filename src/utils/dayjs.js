28524936e8cc4fe24aa9849779054382fa561c6d
olgadeveloper <madonovaolga@icloud.com>
2023-08-17 20:06:14 +0700

    added utc view time

--- src/utils/dayjs.js
+++ src/utils/dayjs.js
@@ -1,24 +1,27 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration.js';
+import utc from 'dayjs/plugin/utc';
+
 dayjs.extend(duration);
+dayjs.extend(utc);
 
 function formatEventDuration (endTime, startTime) {
-  const difference = Math.abs(dayjs(endTime).diff(dayjs(startTime)));
+  const difference = Math.abs(dayjs.utc(endTime).diff(dayjs.utc(startTime)));
   const formatterDifference = dayjs.duration(difference).format('MM[M] DD[D] HH[H] mm[M]');
   return formatterDifference.split(' ').filter((part) => !/00\w/.test(part)).join(' ');
 }
 
 function formatEventDate (date) {
-  const [month, day] = dayjs(date).format('MMM DD').split(' ');
+  const [month, day] = dayjs.utc(date).format('MMM DD').split(' ');
   return `${month.toUpperCase()} ${day}`;
 }
 
 function formatEventDatetime (date) {
-  return dayjs(date).format('YYYY-MM-DD');
+  return dayjs.utc(date).format('YYYY-MM-DD');
 }
 
 function formatEventFromToDate (date) {
-  return dayjs(date).format('HH:mm');
+  return dayjs.utc(date).format('HH:mm');
 }
 
 export { formatEventDuration, formatEventDate, formatEventDatetime, formatEventFromToDate };

0575c623a8dca4f7f45fa7f1fd4947565eba6569
olgadeveloper <madonovaolga@icloud.com>
2023-08-21 18:24:40 +0700

    added value to time tag

--- src/view/form-event/form-event-header-view.js
+++ src/view/form-event/form-event-header-view.js
@@ -85,10 +85,10 @@ function createFormEventDestinationTemplate (destinations) {
 function createFormEventTimeTemplate () {
   return `<div class="event__field-group  event__field-group--time">
   <label class="visually-hidden" for="event-start-time-1">From</label>
-  <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" placeholder="19/03/24 00:00">
+  <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" placeholder="19/03/24 00:00" value="">
   &mdash;
   <label class="visually-hidden" for="event-end-time-1">To</label>
-  <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" placeholder="19/03/24 00:00">
+  <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" placeholder="19/03/24 00:00" value="">
 </div>`;
 }
 
